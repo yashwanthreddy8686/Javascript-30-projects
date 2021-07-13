6d772a3566222c33216f8b567d4677a0d4d3a1a6
Yashwanth reddy <yash868686reddy@gmail.com>
2021-07-13 18:10:51 -0400

    initial commit

--- 22-Follow-along-link-highlighter/script.js
+++ 22-Follow-along-link-highlighter/script.js
@@ -9,7 +9,7 @@ function highlightLink(){
         width: linkCoords.width,
         height:linkCoords.height,
         top: linkCoords.top + window.scrollY,
-        left: linkCoords.left + window.scrollX
+        left: linkCoords. left + window.scrollX
     }
     highlight.style.width = `${coords.width}px`;
     highlight.style.height = `${coords.height}px`;
